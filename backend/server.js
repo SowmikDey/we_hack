14d05f14df0ec22ddbee8f3ab8f7084c3506ea94
Sowmik Dey <154355301+SowmikDey@users.noreply.github.com>
2025-03-23 23:48:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ app.use(cors({
   credentials: true,                                
   methods: ['GET', 'POST', 'PUT', 'DELETE'],         
   allowedHeaders: ['Content-Type', 'Authorization']
-  credentials: true
 }));
 app.use(bodyParser.json());
 app.use(express.json());
